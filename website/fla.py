5782b23c745ae236a990ef387cea0d48bad4ca2b
AchrafAitelquadi <151475064+AchrafAitelquadi@users.noreply.github.com>
2024-06-03 19:15:26 +0100

    Update fla.py

--- website/fla.py
+++ website/fla.py
@@ -1,30 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import pickle
-
+from functions import *
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)  # Enable CORS for all routes
 
 # Load the pipeline from the file
-pipeline_path = r'D:\Dev\DevPfa\Model\my_pipeline.pkl'
-with open(pipeline_path, 'rb') as f:
-    pipe = pickle.load(f)
-
-
-# Define a function to ask questions using the loaded pipeline and show only the first answer
-def ask_question(question):
-
-    prediction = pipe.run(
-        query=question,
-        params={
-            "Retriever": {"top_k": 10},
-            "Reader": {"top_k": 5}
-        }
-    )
-    first_answer = prediction["answers"][0].answer
-    return first_answer
 
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
